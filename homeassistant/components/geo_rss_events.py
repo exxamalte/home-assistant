4f895d69b86309dc16e11f3b8e7765fdcf018396
Malte Franken <exxamalte@users.noreply.github.com>
2018-03-19 08:50:15 +1100

    working version of component and several unit tests

--- homeassistant/components/geo_rss_events.py
+++ homeassistant/components/geo_rss_events.py
@@ -2,8 +2,8 @@
 Generic GeoRSS events service.
 
 Retrieves current events (typically incidents or alerts) in GeoRSS format, and
-shows information on events filtered by distance to the HA instance's location
-and grouped by category.
+publishes information on events filtered by distance to the HA instance's
+location and grouped by category onto the HA event bus.
 
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/geo_rss_events/
@@ -17,8 +17,8 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.feedreader import StoredData, FeedManager
-from homeassistant.const import CONF_URL, CONF_RADIUS, \
-    CONF_UNIT_OF_MEASUREMENT, CONF_NAME, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_URL, CONF_RADIUS, CONF_NAME, \
+    CONF_SCAN_INTERVAL
 from homeassistant.helpers.entity import generate_entity_id
 from homeassistant.helpers.event import track_time_interval
 
@@ -26,57 +26,49 @@ REQUIREMENTS = ['feedparser==5.2.1', 'haversine==0.4.5']
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_CATEGORY = 'category'
-ATTR_DATE_PUBLISHED = 'date_published'
-ATTR_DATE_UPDATED = 'date_updated'
 ATTR_DISTANCE = 'distance'
-ATTR_ID = 'id'
-ATTR_TITLE = 'title'
 
 CONF_CATEGORIES = 'categories'
-CONF_CUSTOM_ATTRIBUTES = 'custom_attributes'
-CONF_CUSTOM_ATTRIBUTES_NAME = 'name'
-CONF_CUSTOM_ATTRIBUTES_REGEXP = 'regexp'
-CONF_CUSTOM_ATTRIBUTES_SOURCE = 'source'
-CONF_CUSTOM_FILTERS = 'custom_filters'
-CONF_CUSTOM_FILTERS_ATTRIBUTE = 'attribute'
-CONF_CUSTOM_FILTERS_REGEXP = 'regexp'
+CONF_ATTRIBUTES = 'attributes'
+CONF_ATTRIBUTES_NAME = 'name'
+CONF_ATTRIBUTES_REGEXP = 'regexp'
+CONF_ATTRIBUTES_SOURCE = 'source'
+CONF_CUSTOM_ATTRIBUTE = 'custom_attribute'
+CONF_FILTERS = 'filters'
+CONF_FILTERS_ATTRIBUTE = 'attribute'
+CONF_FILTERS_REGEXP = 'regexp'
 
-DEFAULT_ICON = 'mdi:alert'
 DEFAULT_NAME = "Event Service"
 DEFAULT_RADIUS_IN_KM = 20.0
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=5)
-DEFAULT_UNIT_OF_MEASUREMENT = 'Events'
 
 DOMAIN = 'geo_rss_events'
 
-CUSTOM_ATTRIBUTES_SCHEMA = vol.Schema({
-    vol.Required(CONF_CUSTOM_ATTRIBUTES_NAME): cv.string,
-    vol.Required(CONF_CUSTOM_ATTRIBUTES_SOURCE): cv.string,
-    vol.Required(CONF_CUSTOM_ATTRIBUTES_REGEXP): cv.string,
+ATTRIBUTES_SCHEMA = vol.Schema({
+    vol.Required(CONF_ATTRIBUTES_NAME): cv.string,
+    vol.Required(CONF_ATTRIBUTES_SOURCE): cv.string,
+    vol.Required(CONF_ATTRIBUTES_REGEXP): cv.string,
 })
 
-CUSTOM_FILTERS_SCHEMA = vol.Schema({
-    vol.Required(CONF_CUSTOM_FILTERS_ATTRIBUTE): cv.string,
-    vol.Required(CONF_CUSTOM_FILTERS_REGEXP): cv.string,
+FILTERS_SCHEMA = vol.Schema({
+    vol.Required(CONF_FILTERS_ATTRIBUTE): cv.string,
+    vol.Required(CONF_FILTERS_REGEXP): cv.string,
 })
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.All(cv.ensure_list, [{
         vol.Required(CONF_URL): cv.string,
-        vol.Optional(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM): vol.Coerce(
-            float),
+        vol.Optional(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM):
+            vol.Coerce(float),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL):
             cv.time_period,
         vol.Optional(CONF_CATEGORIES, default=[]):
             vol.All(cv.ensure_list, [cv.string]),
-        vol.Optional(CONF_CUSTOM_ATTRIBUTES,
-                     default=[]): vol.All(cv.ensure_list,
-                                          [CUSTOM_ATTRIBUTES_SCHEMA]),
-        vol.Optional(CONF_CUSTOM_FILTERS,
-                     default=[]): vol.All(cv.ensure_list,
-                                          [CUSTOM_FILTERS_SCHEMA]),
+        vol.Optional(CONF_ATTRIBUTES, default=[]):
+            vol.All(cv.ensure_list, [ATTRIBUTES_SCHEMA]),
+        vol.Optional(CONF_FILTERS, default=[]):
+            vol.All(cv.ensure_list, [FILTERS_SCHEMA]),
     }])
 }, extra=vol.ALLOW_EXTRA)
 
@@ -93,15 +85,15 @@ def setup(hass, config):
         radius_in_km = feed_config.get(CONF_RADIUS)
         name = feed_config.get(CONF_NAME)
         scan_interval = feed_config.get(CONF_SCAN_INTERVAL)
-        custom_attributes_definition = feed_config.get(CONF_CUSTOM_ATTRIBUTES)
-        custom_filters_definition = feed_config.get(CONF_CUSTOM_FILTERS)
-        #categories = config[DOMAIN].get(CONF_CATEGORIES)
+        attributes_definition = feed_config.get(CONF_ATTRIBUTES)
+        filters_definition = feed_config.get(CONF_FILTERS)
         _LOGGER.debug("latitude=%s, longitude=%s, url=%s, radius=%s",
                       home_latitude, home_longitude, url, radius_in_km)
+        # Intantiate feed manager for each configured feed.
         GeoRssFeedManager(hass, storage, scan_interval, name, home_latitude,
                           home_longitude, url, radius_in_km,
-                          custom_attributes_definition,
-                          custom_filters_definition)
+                          attributes_definition,
+                          filters_definition)
     return True
 
 
@@ -109,21 +101,21 @@ class GeoRssFeedManager(FeedManager):
     """Feed Manager for Geo RSS feeds."""
 
     def __init__(self, hass, storage, scan_interval, name, home_latitude,
-                 home_longitude, url, radius_in_km,
-                 custom_attributes_definition, custom_filters_definition):
+                 home_longitude, url, radius_in_km, attributes_definition,
+                 filters_definition):
         self._scan_interval = scan_interval
         super().__init__(url, hass, storage)
         self._name = name
         self._home_coordinates = [home_latitude, home_longitude]
         self._radius_in_km = radius_in_km
-        self._custom_attributes_definition = custom_attributes_definition
-        self._custom_filters_definition = custom_filters_definition
+        self._attributes_definition = attributes_definition
+        self._filters_definition = filters_definition
         entity_id = generate_entity_id('{}', name, hass=hass)
-        self._event_type_id = entity_id
+        self._event_type = entity_id
         self._feed_id = entity_id
 
     def _init_regular_updates(self, hass):
-        """Schedule regular updates."""
+        """Schedule regular updates based on configured time interval ."""
         track_time_interval(hass, lambda now: self._update(),
                             self._scan_interval)
 
@@ -147,35 +139,35 @@ class GeoRssFeedManager(FeedManager):
                     # Add distance value as a new attribute
                     entry.update({ATTR_DISTANCE: distance})
                     # Compute custom attributes.
-                    for definition in self._custom_attributes_definition:
-                        if hasattr(entry,
-                                   definition[CONF_CUSTOM_ATTRIBUTES_SOURCE]):
+                    for definition in self._attributes_definition:
+                        if hasattr(entry, definition[CONF_ATTRIBUTES_SOURCE]):
                             # Use 'search' to allow for matching anywhere in
                             # the source attribute.
                             match = re.search(
-                                definition[CONF_CUSTOM_ATTRIBUTES_REGEXP],
-                                entry[definition[
-                                    CONF_CUSTOM_ATTRIBUTES_SOURCE]])
-                            entry.update({definition[
-                                CONF_CUSTOM_ATTRIBUTES_NAME]: None if not match \
-                                else match.group('custom_attribute')})
+                                definition[CONF_ATTRIBUTES_REGEXP],
+                                entry[definition[CONF_ATTRIBUTES_SOURCE]])
+                            entry.update({definition[CONF_ATTRIBUTES_NAME]:
+                                         '' if not match else match.group(
+                                                  CONF_CUSTOM_ATTRIBUTE)})
                         else:
                             _LOGGER.warning("No attribute '%s' found",
-                                            definition[
-                                                CONF_CUSTOM_ATTRIBUTES_SOURCE])
-                            entry.update({definition[CONF_CUSTOM_ATTRIBUTES_NAME]: None})
+                                            definition[CONF_ATTRIBUTES_SOURCE])
+                            # Add empty string to allow for applying filter
+                            # rules.
+                            entry.update(
+                                {definition[CONF_ATTRIBUTES_NAME]: ''})
                     # Run custom filters if defined.
                     keep_entry = True
-                    if self._custom_filters_definition:
-                        for definition in self._custom_filters_definition:
-                            if hasattr(entry, definition[CONF_CUSTOM_FILTERS_ATTRIBUTE]):
+                    if self._filters_definition:
+                        for definition in self._filters_definition:
+                            if hasattr(entry, definition[
+                                   CONF_FILTERS_ATTRIBUTE]):
                                 match = re.match(
-                                    definition[CONF_CUSTOM_FILTERS_REGEXP],
-                                    entry.get(definition[
-                                        CONF_CUSTOM_FILTERS_ATTRIBUTE]))
-                                # If the attribute does not match, immediately return
-                                # None value to eliminate entry, otherwise continue with
-                                # the filter loop.
+                                    definition[CONF_FILTERS_REGEXP],
+                                    entry.get(
+                                        definition[CONF_FILTERS_ATTRIBUTE]))
+                                # If the attribute does not match, immediately
+                                # drop out of loop to eliminate the entry.
                                 if not match:
                                     _LOGGER.debug(
                                         "Entry %s does not match filter %s",
